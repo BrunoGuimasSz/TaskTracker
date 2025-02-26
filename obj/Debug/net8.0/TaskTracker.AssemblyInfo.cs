3830db32f871fbd9ac321645a1527d84057cffe7
BrrunoGuimasSz <brunoguimaraes.guigui@gmail.com>
2025-02-26 11:33:01 -0300

    add readme

--- obj/Debug/net8.0/TaskTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c6b9fe113a61688120a488a9ca1473dc19a8996")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31eca6b910b32de86727dcdb08a154df6365a1cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
